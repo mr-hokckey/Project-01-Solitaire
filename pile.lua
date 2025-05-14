29768b07fba0fa79f4e43ab51ff0e7fc7137ed67
mr-hokckey <140927221+mr-hokckey@users.noreply.github.com>
2025-05-14 16:01:46 -0700

    PROJECT 2: refactor pile push, new sprites

--- pile.lua
+++ pile.lua
@@ -80,41 +80,32 @@ end
 -- Insert the card at the end of the table,
 -- move the card to the proper position, and return true. or return false if unsuccessful.
 function PileClass:push(cards)
-  if self.type == PILE_TYPE.TABLEAU then
-    if #self.cards == 0 then
-      if math.fmod(cards[1]:getValue(), 13) == 12 then
-        for _, card in ipairs(cards) do
-          table.insert(self.cards, card)
-          card.location = self.name
-        end
-        return true
-      end
-    elseif cards[1]:canStackTableau(self.cards[#self.cards]) then
-      for _, card in ipairs(cards) do
-        table.insert(self.cards, card)
-        card.location = self.name
-      end
-      return true
-    end
-  elseif self.type == PILE_TYPE.FOUNDATION then
-    if #self.cards == 0 then
-      if math.fmod(cards[1]:getValue(), 13) == 0 then
-        table.insert(self.cards, cards[1])
-        cards[1].location = self.name
-        return true
-      end
-    elseif #cards == 1 and cards[1]:canStackFoundation(self.cards[#self.cards]) then
-      table.insert(self.cards, cards[1])
-      cards[1].location = self.name
-      return true
-    end
-  -- if we're pushing to the wastepile, we're assuming we're getting these from the deck.
-  elseif self.type == PILE_TYPE.WASTE then
+  if self:isLegalPush(cards) then
     for _, card in ipairs(cards) do
       table.insert(self.cards, card)
       card.location = self.name
-      return true
     end
+    return true
+  end
+
+  return false
+end
+
+-- new helper function for the refactor. Checks whether or not this is a legal move.
+function PileClass:isLegalPush(cards)
+  if self.type == PILE_TYPE.TABLEAU then
+    return (#self.cards == 0 and math.fmod(cards[1]:getValue(), 13) == 12) or 
+    cards[1]:canStackTableau(self.cards[#self.cards])
+
+  elseif self.type == PILE_TYPE.FOUNDATION then
+    return (#self.cards == 0 and math.fmod(cards[1]:getValue(), 13) == 0) or
+    (#cards == 1 and cards[1]:canStackFoundation(self.cards[#self.cards]))
+
+  -- if we're pushing to the wastepile, we're assuming we're getting these from the deck.
+  -- even though this returns true, it is still impossible to drag cards to the wastepile,
+  -- since checkForMouseOver() will return false before we even get here.
+  elseif self.type == PILE_TYPE.WASTE then
+    return true
   end
 
   return false
@@ -133,23 +124,23 @@ end
 
 -- return T/F if the mouse is in the drop zone for the pile. 
 function PileClass:checkForMouseOver()
-  local isMouseOver = 
-    love.mouse.getX() > self.position.x and
-    love.mouse.getX() < self.position.x + 64 and
-    love.mouse.getY() > self.position.y + (#self.cards-1) * 24 * self.type and
-    love.mouse.getY() < self.position.y + (#self.cards-1) * 24 * self.type + 96
-    
-  if self.type == PILE_TYPE.FOUNDATION then
+  local isMouseOver = false
+
+  if self.type == PILE_TYPE.TABLEAU then
+    isMouseOver = 
+      love.mouse.getX() > self.position.x and
+      love.mouse.getX() < self.position.x + 64 and
+      love.mouse.getY() > self.position.y + (#self.cards-1) * 24 * self.type and
+      love.mouse.getY() < self.position.y + (#self.cards-1) * 24 * self.type + 96
+  elseif self.type == PILE_TYPE.FOUNDATION then
     isMouseOver = 
       love.mouse.getX() > self.position.x and
       love.mouse.getX() < self.position.x + 64 and
       love.mouse.getY() > self.position.y and
       love.mouse.getY() < self.position.y + 96
   end
-  
 
-  -- because you can't drag something to the wastepile
-  if isMouseOver and self.type ~= PILE_TYPE.WASTE then 
+  if isMouseOver then 
     return self.name
   else
     return "FALSE"
